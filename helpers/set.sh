1f60147195e14e722ac393b4fe1627ef48a63efe
techguy16 <88870951+techguy16@users.noreply.github.com>
2025-09-09 13:36:22 +1200

    Move all name updates to set

--- helpers/set.sh
+++ helpers/set.sh
@@ -7,4 +7,6 @@ if [[ "$1" == "google-chrome"* ]]; then
     set_title "Google Chrome"
 elif [[ "$1" == "epiphany" ]] || [[ "$1" == "epiphany-browser" ]]; then
     set_title "GNOME Web"
+elif [[ "$1" == "waydroid" ]] && [[ "$4" == "com.mojang.minecraftedu" ]]; then
+    set_title "Minecraft Education"
 fi
