d0f158eca422bc94e5c6757f4679ac195b7a4083
techguy16 <88870951+techguy16@users.noreply.github.com>
2025-09-09 13:02:54 +1200

    Less  chars

--- helpers/set.sh
+++ helpers/set.sh
@@ -3,7 +3,7 @@
 set_title() {
     wmctrl -r "Weston Compositor - screen0" -N "$1"
 }
-if [[ "$1" == "google-chrome-stable" ]] || [[ "$1" == "google-chrome-beta" ]] || [[ "$1" == "google-chrome-dev" ]] || [[ "$1" == "google-chrome-canary" ]]; then
+if [[ "$1" == "google-chrome"* ]]; then
     set_title "Google Chrome"
 elif [[ "$1" == "epiphany" ]] || [[ "$1" == "epiphany-browser" ]]; then
     set_title "GNOME Web"
